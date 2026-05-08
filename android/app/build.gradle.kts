1c92218ee2cef0a4e3c779ee71e25f89eafd08c5
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2026-05-09 02:24:59 +0400

    fix: Update package name to com.tapylabs.neuralq for Play Store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.neuralq.neuralq"
+    namespace = "com.tapylabs.neuralq"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.neuralq.neuralq"
+        applicationId = "com.tapylabs.neuralq"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
